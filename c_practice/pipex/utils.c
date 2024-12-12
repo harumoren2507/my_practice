224f93a9f534f52c3b4b38795711f59b96a3b035
harumoren2507 <kuroshiro0725@gmail.com>
2024-12-12 20:47:28 +0900

    a

--- c_practice/pipex/utils.c
+++
@@ -1,5 +0,0 @@
-void  err_message(char *err_msg)
-{
-  perror(err_msg);
-  exit(EXIT_FAILURE);
-}
--- c_practice/pipex_practice/dup2.c
+++ c_practice/pipex_practice/dup2.c
@@ -6,10 +6,10 @@ int main(void)
 {
   int fd;
   fd = open("example.txt2", O_WRONLY | O_CREAT, 0644);
+  close(STDIN_FILENO);
   dup2(fd, STDOUT_FILENO);
   close(fd);
   printf("This is printed in example.txt!\n");
-
   return (0);
 }
 
